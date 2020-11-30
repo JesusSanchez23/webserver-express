50f4232ff2d290da5f3551d69e7d023b9f02ecdc
Jesus <jesus8347@gmail.com>
2020-11-29 22:06:56 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'))
+
+//Express HBS
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//helpers
+
+
+app.get('/', function(req, res) {
+    res.render('home', {
+        nombre: 'Jesus',
+
+    })
+})
+app.get('/about', function(req, res) {
+    res.render('about', {
+
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`Escuchando ${port}`);
+})
